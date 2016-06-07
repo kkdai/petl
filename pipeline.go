d870524f10aa129714082aefc735e97e15dd38e1
Evan Lin <evanslin@gmail.com>
2016-06-07 11:51:19 +0800

    add pipeline

---
+++ pipeline.go
@@ -0,0 +1,47 @@
+package main
+
+import "sync"
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int, len(nums))
+	for _, n := range nums {
+		out <- n
+	}
+	close(out)
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int, len(in))
+	for n := range in {
+		out <- n * n
+	}
+	close(out)
+	return out
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
---
+++ pipeline_test.go
@@ -0,0 +1,65 @@
+package pipeline
+
+import "testing"
+
+func TestPipe(t *testing.T) {
+	// Set up the pipeline.
+	c := gen(2, 3)
+	out := sq(c)
+
+	// Consume the output.
+	if 4 != <-out {
+		t.Error("eror 1")
+	}
+	if 9 != <-out {
+		t.Error("eror 1")
+	}
+}
+
+func TestPipe2(t *testing.T) {
+	out := sq(sq(gen(2, 3)))
+
+	if 16 != <-out {
+		t.Error("eror 2")
+	}
+	if 81 != <-out {
+		t.Error("eror 2")
+	}
+}
+
+func TestPipe3(t *testing.T) {
+	in := gen(2, 3)
+
+	// Distribute the sq work across two goroutines that both read from in.
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// Consume the merged output from c1 and c2.
+	out := merge(c1, c2)
+
+	if 4 != <-out {
+		t.Error("eror 3")
+	}
+	if 9 != <-out {
+		t.Error("eror 3")
+	}
+}
+
+// func TestPipe4(t *testing.T) {
+// 	in := gen(2, 3)
+
+// 	// Distribute the sq work across two goroutines that both read from in.
+// 	c1 := sq(in)
+// 	c2 := sq(in)
+
+// 	done := make(chan struct{}, 2)
+// 	// Consume the merged output from c1 and c2.
+// 	out := merge(done, c1, c2)
+
+// 	if 4 != <-out {
+// 		t.Error("eror 3")
+// 	}
+// 	if 9 != <-out {
+// 		t.Error("eror 3")
+// 	}
+// }
